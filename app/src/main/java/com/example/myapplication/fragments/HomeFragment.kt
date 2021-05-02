d056efd4fc6f0b62a1487a151f642e5ac23d0f36
geoffroy <geoffroy.h@hotmail.fr>
2021-05-02 17:02:34 +0200

    mise à jour nouvelle version

--- app/src/main/java/com/example/myapplication/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/HomeFragment.kt
@@ -5,15 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.adapter.PlantAdapter
 
 class HomeFragment :Fragment() {
     override fun
             onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        return inflater?.inflate(R.layout.fragment_home, container, false)
+        val view = inflater?.inflate(R.layout.fragment_home, container, false)
+        //recuperer recyclerview
+        val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_Recycler_view)
+        horizontalRecyclerView.adapter = PlantAdapter()
+
+        return view
     }
 }
