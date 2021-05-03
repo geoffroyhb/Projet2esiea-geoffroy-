57556c4f1d32082af8d090771080fb2b8d5f37ec
geoffroy <geoffroy.h@hotmail.fr>
2021-05-03 09:31:28 +0200

    mise à jour nouvelle version

--- app/src/main/java/com/example/myapplication/adapter/PlantAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapter/PlantAdapter.kt
@@ -1,5 +1,8 @@
 package com.example.myapplication.adapter
 
+import android.annotation.SuppressLint
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,11 +10,13 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 
- class PlantAdapter: RecyclerView.Adapter<PlantAdapter.ViewHolder>() {
+
+ class PlantAdapter () : RecyclerView.Adapter<PlantAdapter.ViewHolder>() {
      //boite pou ranger tout les composants
      class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
          val plantImage = view.findViewById<ImageView>(R.id.image_item)
      }
+
 
      override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view =LayoutInflater.from(parent.context).inflate(R.layout.item_horizontable_plant, parent,false)
@@ -19,9 +24,14 @@ import com.example.myapplication.R
      }
 
 
-     override fun onBindViewHolder(holder: ViewHolder, position: Int) {}
 
-     override fun getItemCount(): Int 5
+     override fun getItemCount(): Int {
+         return 5
+     }
+
+     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+     }
 
 
  }
