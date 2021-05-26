ff4b4785f7ea4430e37b67fc83897f2207bf514b
geoffroyhb <haftman-boutel@et.esiea.fr>
2021-05-26 23:57:41 +0200

    mise à jour nouvelle version

--- app/src/main/java/com/example/myapplication/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/HomeFragment.kt
@@ -32,6 +32,41 @@ class HomeFragment :Fragment() {
             imageUrl = "https://media.istockphoto.com/photos/flowers-red-rose-isolated-on-white-background-picture-id1191892713?s=612x612",
                 liked = false
         ))
+
+        // enregistrer une seconde  plante (lavande)
+        plantList.add(
+            Plantmodel(
+                name = "lavande",
+                description = "violet écarlate ",
+                imageUrl = "https://media.istockphoto.com/photos/garden-with-the-flourishing-lavender-picture-id1140984793?s=612x612",
+                liked = false
+            ))
+
+
+        // enregistrer une troisième plante (pissenlit)
+        plantList.add(
+            Plantmodel(
+                name = "pissenlit",
+                description = "petit pissenlit",
+                imageUrl = "https://media.istockphoto.com/photos/two-dandelions-with-leaves-picture-id1142487263?s=612x612",
+                liked = false
+            ))
+
+
+        // enregistrer une quatrième plante (nénuphar )
+        plantList.add(
+            Plantmodel(
+                name = "nénuphar",
+                description = "small nénuphar",
+                imageUrl = "https://media.istockphoto.com/photos/waterlilies-picture-id115015104?s=612x612",
+                liked = false
+            ))
+
+
+
+
+
+
         //recuperer recyclerview
         val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_Recycler_view)
         horizontalRecyclerView.adapter = PlantAdapter(R.layout.item_horizontable_plant)
